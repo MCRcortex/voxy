ada7af983f4d4032fedea8036ec8f4652d1ac595
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2024-02-12 12:15:44 +1000

    Added conditional config idea

---
+++ src/main/java/me/cortex/voxy/common/storage/other/ConditionalStorageBackendConfig.java
@@ -0,0 +1,23 @@
+package me.cortex.voxy.common.storage.other;
+
+import me.cortex.voxy.common.storage.StorageBackend;
+import me.cortex.voxy.common.storage.config.ConfigBuildCtx;
+import me.cortex.voxy.common.storage.config.Serialization;
+import me.cortex.voxy.common.storage.config.StorageConfig;
+import org.apache.commons.lang3.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//A conditional storage backend depending on build time config, this enables conditional backends depending on the
+// dimension as an example
+public class ConditionalStorageBackendConfig extends StorageConfig {
+    @Override
+    public StorageBackend build(ConfigBuildCtx ctx) {
+        throw new NotImplementedException();
+    }
+
+    public static String getConfigTypeName() {
+        return "ConditionalConfig";
+    }
+}
