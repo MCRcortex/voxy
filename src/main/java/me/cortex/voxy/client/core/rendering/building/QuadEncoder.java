061058804d07a280e495b70b4a100f350af7e294
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2024-03-04 11:17:00 +1000

    Attempt to improve mesher just a small ammount

--- src/main/java/me/cortex/voxy/client/core/rendering/building/QuadEncoder.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/building/QuadEncoder.java
@@ -11,30 +11,25 @@ public class QuadEncoder {
         if (Mesher2D.getW(encodedMeshedData) > 16 || Mesher2D.getH(encodedMeshedData) > 16) {
             throw new IllegalStateException("Width or height > 16");
         }
-        int out = face;
-        out |= switch (face >> 1) {
-            case 0 ->
-                    (Mesher2D.getX(encodedMeshedData) << 21) |
-                            (otherAxis << 16) |
-                            (Mesher2D.getZ(encodedMeshedData) << 11) |
-                            ((Mesher2D.getW(encodedMeshedData)-1) << 7) |
-                            ((Mesher2D.getH(encodedMeshedData)-1) << 3);
+        int dat = face;
+        dat |= ((Mesher2D.getW(encodedMeshedData) - 1) << 7) |
+                ((Mesher2D.getH(encodedMeshedData) - 1) << 3);
 
-            case 1 ->
-                    (Mesher2D.getX(encodedMeshedData) << 21) |
-                            (Mesher2D.getZ(encodedMeshedData) << 16) |
-                            (otherAxis << 11) |
-                            ((Mesher2D.getW(encodedMeshedData)-1) << 7) |
-                            ((Mesher2D.getH(encodedMeshedData)-1) << 3);
-
-            case 2 ->
-                    (otherAxis << 21) |
-                            (Mesher2D.getX(encodedMeshedData) << 16) |
-                            (Mesher2D.getZ(encodedMeshedData) << 11) |
-                            ((Mesher2D.getW(encodedMeshedData)-1) << 7) |
-                            ((Mesher2D.getH(encodedMeshedData)-1) << 3);
-            default -> throw new IllegalStateException("Unexpected value: " + (face >> 1));
-        };
-        return out;
+        if (face>>1 == 0) {
+            return dat |
+                   (Mesher2D.getX(encodedMeshedData) << 21) |
+                   (otherAxis << 16) |
+                   (Mesher2D.getZ(encodedMeshedData) << 11);
+        }
+        if (face>>1 == 1) {
+            return dat |
+                   (Mesher2D.getX(encodedMeshedData) << 21) |
+                   (Mesher2D.getZ(encodedMeshedData) << 16) |
+                   (otherAxis << 11);
+        }
+        return dat |
+               (otherAxis << 21) |
+               (Mesher2D.getX(encodedMeshedData) << 16) |
+               (Mesher2D.getZ(encodedMeshedData) << 11);
     }
 }
