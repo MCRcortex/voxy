7ec5729a1e1e4374ef8ca6b42b4e4d3cdd2a494a
julianroldan13579-ops <julianroldan13579@gmail.com>
2026-03-29 11:37:46 -0700

    Revert "f"

--- src/main/java/me/cortex/voxy/client/core/rendering/building/RenderDataFactory.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/building/RenderDataFactory.java
@@ -63,7 +63,7 @@ public class RenderDataFactory {
 
     private int quadCount = 0;
 
-    private final OccupancySet occupancy;
+    private final OccupancySet occupancy = new OccupancySet();
 
     //Wont work for double sided quads
     private final class Mesher extends ScanMesher2D {
@@ -186,16 +186,8 @@ public class RenderDataFactory {
     private final Mesher seondaryblockMesher = new Mesher();//Used for dual non-opaque geometry
 
     public RenderDataFactory(WorldEngine world, ModelFactory modelManager, boolean emitMeshlets) {
-        this(world, modelManager, emitMeshlets, false);
-    }
-    public RenderDataFactory(WorldEngine world, ModelFactory modelManager, boolean emitMeshlets, boolean generateOccupancy) {
         this.world = world;
         this.modelMan = modelManager;
-        if (generateOccupancy && BUILD_OCCUPANCY_SET) {
-            this.occupancy = new OccupancySet();
-        } else {
-            this.occupancy = null;
-        }
     }
 
     private static long getQuadTyping(long metadata) {//2 bits
@@ -1632,9 +1624,8 @@ public class RenderDataFactory {
                 }
             }
         }
-        if (this.occupancy != null) {
-            this.occupancy.reset();
-        }
+
+        this.occupancy.reset();
 
         this.minX = Integer.MAX_VALUE;
         this.minY = Integer.MAX_VALUE;
@@ -1669,7 +1660,7 @@ public class RenderDataFactory {
         }
 
         //We only care if we have quads
-        if (this.occupancy != null && section.lvl == 0 /*only generate occupancy for lowest lod level*/ && this.quadCount != 0 && (flags&1) != 0) {
+        if (BUILD_OCCUPANCY_SET && this.quadCount != 0 && (flags&1) != 0) {
             this.buildOccupancy();
         }
 
@@ -1708,7 +1699,7 @@ public class RenderDataFactory {
         aabb |= (this.maxZ-this.minZ-1)<<25;
 
         MemoryBuffer occupancy = null;
-        if (this.occupancy != null && !this.occupancy.isEmpty()) {
+        if (BUILD_OCCUPANCY_SET && !this.occupancy.isEmpty()) {
             occupancy = new MemoryBuffer(this.occupancy.writeSize());
             this.occupancy.write(occupancy.address, false);
         }
