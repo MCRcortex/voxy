bf828946c8fefb0792f22b2dd402df3ea4d6274b
KaptainWutax <36993134+KaptainWutax@users.noreply.github.com>
2026-04-25 13:52:15 -0400

    fix same-model non-opaque face culling

--- src/main/java/me/cortex/voxy/client/core/rendering/building/RenderDataFactory.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/building/RenderDataFactory.java
@@ -353,7 +353,8 @@ public class RenderDataFactory {
     private static final long LM = (0xFFL<<55);
 
     private static boolean shouldMeshNonOpaqueBlockFace(int face, long quad, long meta, long neighborQuad, long neighborMeta) {
-        if (((quad^neighborQuad)&(0xFFFFL<<26))==0 && (DISABLE_CULL_SAME_OCCLUDES || (ModelQueries.cullsSame(meta)||ModelQueries.faceOccludes(meta, face)))) return false;//This is a hack, if the neigbor and this are the same, dont mesh the face// TODO: FIXME
+        if (((quad^neighborQuad)&(0xFFFFL<<26))==0 && ModelQueries.faceExists(neighborMeta, face ^ 1)
+                && (DISABLE_CULL_SAME_OCCLUDES || ModelQueries.cullsSame(meta))) return false;//This is a hack, if the neigbor and this are the same, dont mesh the face// TODO: FIXME
         if (!ModelQueries.faceExists(meta, face)) return false;//Dont mesh if no face
         if (ModelQueries.faceCanBeOccluded(meta, face)) //TODO: maybe enable this
           if (ModelQueries.faceOccludes(neighborMeta, face^1)) return false;
